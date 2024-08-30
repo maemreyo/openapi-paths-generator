5f435fb0c08eadaf014e7d6d3961a4e647ea7f86
Matthew Ngo <ngonhuthanhtrung1409@gmail.com>
2024-08-31 00:28:28 +0700

    Remove dtype

--- dist/utils/logging.d.ts
+++
@@ -1,6 +0,0 @@
-import winston from "winston";
-export declare const logger: winston.Logger;
-export declare const logStart: () => void;
-export declare const logCompletion: () => void;
-export declare const logFileCreation: (filePath: string) => void;
-//# sourceMappingURL=logging.d.ts.map
--- dist/utils/output.d.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Handles the preparation of the output directory.
- * @param outputDir - The base output directory.
- * @param customName - Optional custom name for the apiPaths directory.
- * @returns The path to the module output directory.
- */
-export declare const prepareOutputDirectory: (outputDir: string, customName?: string) => string;
-//# sourceMappingURL=output.d.ts.map
