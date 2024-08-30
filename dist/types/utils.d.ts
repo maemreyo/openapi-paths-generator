b3891ff54be7949fbbd1cec1a8a65c98ce5c35ca
Matthew Ngo <ngonhuthanhtrung1409@gmail.com>
2024-08-30 22:49:53 +0700

    Change build type

---
+++ dist/types/utils.d.ts
@@ -0,0 +1,10 @@
+/// <reference types="node" />
+export interface FileOperationResult {
+    success: boolean;
+    message: string;
+}
+export interface FileReadOptions {
+    encoding?: BufferEncoding;
+    flag?: string;
+}
+//# sourceMappingURL=utils.d.ts.map
