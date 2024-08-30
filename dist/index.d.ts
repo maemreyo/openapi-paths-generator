7562e87873a4c59060516c8f724ab29d6bf6a5ed
Matthew Ngo <ngonhuthanhtrung1409@gmail.com>
2024-08-30 21:57:53 +0700

    Remove dtype

--- dist/index.d.ts
+++
@@ -1,7 +0,0 @@
-import { GenerateApiPathsOptions } from "./types";
-/**
- * Generate API paths based on an OpenAPI spec file.
- * @param options - The options object containing all necessary parameters.
- */
-declare const generateApiPathsFromSpec: (options: GenerateApiPathsOptions) => void;
-export default generateApiPathsFromSpec;
--- dist/utils/processing.d.ts
+++
@@ -1,6 +0,0 @@
-/**
- * Processes and writes the module files in parallel with incremental updates.
- * @param moduleOutputDir - The directory where the module files will be written.
- * @param apiPaths - The generated API paths object, organized by modules.
- */
-export declare const processModules: (moduleOutputDir: string, apiPaths: Record<string, Record<string, string>>) => Promise<void>;
